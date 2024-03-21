c85e43d190f953a0353aeae4ea7b3355cfff590e
rjrhaynes <122994122+rjrhaynes@users.noreply.github.com>
2024-03-21 14:10:17 +0000

    bbb

---
+++ script.js
@@ -0,0 +1,306 @@
+let startScreen = document.querySelector('.main');
+console.log(startScreen);
+
+let start = document.querySelector(".start");
+console.log(start);
+
+let questionOne = document.querySelector('.question-one');
+console.log(questionOne);
+
+let questionTwo = document.querySelector('.question-two');
+console.log(questionTwo);
+
+let questionThree = document.querySelector('.question-three');
+console.log(questionThree);
+
+let questionFour = document.querySelector('.question-four');
+console.log(questionFour);
+
+let questionFive = document.querySelector('.question-five');
+console.log(questionFive);
+
+let resultOne = document.querySelector('.result-one');
+console.log(resultOne);
+
+let resultTwo = document.querySelector('.result-two');
+console.log(resultTwo);
+
+let resultThree = document.querySelector('.result-three');
+console.log(resultThree);
+
+let resultFour = document.querySelector('.result-four');
+console.log(resultFour);
+
+let questionTen = document.querySelector('.question-ten');
+console.log(questionTen);
+
+let optionOneOne = document.querySelector(".option-one-one");
+console.log(optionOneOne);
+
+let optionOneTwo = document.querySelector(".option-one-two");
+console.log(optionOneTwo);
+
+let optionOneThree = document.querySelector(".option-one-three");
+console.log(optionOneThree);
+
+let optionOneFour = document.querySelector(".option-one-four");
+console.log(optionOneFour);
+
+let optionTwoOne = document.querySelector(".option-two-one");
+console.log(optionTwoOne);
+
+let optionTwoTwo = document.querySelector(".option-two-two");
+console.log(optionTwoTwo);
+
+let optionTwoThree = document.querySelector(".option-two-three");
+console.log(optionTwoThree);
+
+let optionTwoFour = document.querySelector(".option-two-four");
+console.log(optionTwoFour);
+
+let optionThreeOne = document.querySelector(".option-three-one");
+console.log(optionThreeOne);
+
+let optionThreeTwo = document.querySelector(".option-three-two");
+console.log(optionThreeTwo);
+
+let optionThreeThree = document.querySelector(".option-three-three");
+console.log(optionThreeThree);
+
+let optionThreeFour = document.querySelector(".option-three-four");
+console.log(optionThreeFour);
+
+let optionFourOne = document.querySelector(".option-four-one");
+console.log(optionFourOne);
+
+let optionFourTwo = document.querySelector(".option-four-two");
+console.log(optionFourTwo);
+
+let optionFourThree = document.querySelector(".option-four-three");
+console.log(optionFourThree);
+
+let optionFourFour = document.querySelector(".option-four-four");
+console.log(optionFourFour);
+
+let optionFiveOne = document.querySelector(".option-five-one");
+console.log(optionFiveOne);
+
+let optionFiveTwo = document.querySelector(".option-five-two");
+console.log(optionFiveTwo);
+
+let optionFiveThree = document.querySelector(".option-five-three");
+console.log(optionFiveThree);
+
+let optionFiveFour = document.querySelector(".option-five-four");
+console.log(optionFiveFour);
+
+let optionSix = document.querySelector(".option-six");
+console.log(optionSix);
+
+let optionSeven = document.querySelector(".option-seven");
+console.log(optionSeven);
+
+let optionEight = document.querySelector(".option-eight");
+console.log(optionEight);
+
+let optionNine = document.querySelector(".option-nine");
+console.log(optionNine);
+
+let optionTen = document.querySelector(".option-ten");
+console.log(optionTen);
+
+let optionEleven = document.querySelector(".option-eleven");
+console.log(optionEleven);
+
+let optionTwelve = document.querySelector(".option-twelve");
+console.log(optionTwelve);
+
+
+
+
+start.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'block';
+    console.log("question-one"); 
+  });
+
+
+optionOneOne.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'block';
+    console.log("should be on military question"); 
+  });
+
+  optionOneTwo.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'block';
+    console.log("question-two resturant 2"); 
+  });
+
+  optionOneThree.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'block';
+    console.log("question-two nature 3"); 
+  });
+
+  optionOneFour.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'block';
+    console.log("question-two home 4"); 
+  });
+
+  optionTwoOne.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'block';
+    console.log("should be seeing decision question"); 
+  });
+
+  optionTwoTwo.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'block';
+    console.log("question-three marine 2"); 
+  });
+
+  optionTwoThree.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'block';
+    console.log("question-three navy 3"); 
+  });
+
+  optionTwoFour.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'block';
+    console.log("question-three army 1"); 
+  });
+
+  optionThreeOne.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'block';
+    console.log("Should be seeing food question"); 
+  });
+
+  optionThreeTwo.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'block';
+    console.log("question-Four 2"); 
+  });
+
+  optionThreeThree.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'block';
+    console.log("question-Four 3"); 
+  });
+
+  optionThreeFour.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'block';
+    console.log("question-Four 4"); 
+  });
+
+  optionFourOne.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'none';
+    questionFive.style.display = 'block';
+    console.log("should be on anime question"); 
+  });
+
+  optionFourTwo.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'none';
+    questionFive.style.display = 'block';
+    console.log("question-five 2"); 
+  });
+
+  optionFourThree.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'none';
+    questionFive.style.display = 'block';
+    console.log("question-five 3"); 
+  });
+
+  optionFourFour.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'none';
+    questionFive.style.display = 'block';
+    console.log("question-five 4"); 
+  });
+  
+  optionFiveOne.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'none';
+    questionFive.style.display = 'none';
+    resultOne.style.display = 'block';
+    console.log("moku"); 
+  });
+
+  optionFiveTwo.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'none';
+    questionFive.style.display = 'none';
+    resultTwo.style.display = 'block';
+    console.log("gomu"); 
+  });
+
+  optionFiveThree.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'none';
+    questionFive.style.display = 'none';
+    resultThree.style.display = 'block';
+    console.log("mera"); 
+  });
+  optionFiveFour.addEventListener('click', function() {
+    startScreen.style.display = 'none';
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'none';
+    questionFive.style.display = 'none';
+    resultFour.style.display = 'block';
+    console.log("Dragon"); 
+  });
+
